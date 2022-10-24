0a014d449fb26aa20846f83d615060b35f08eeb5
ClementNjeru <njeruclement36@gmail.com>
2022-10-24 22:36:16 +0300

    creates the homepage and the footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import Homepage from './components/Homepage';
+import Cohorts from './components/Cohorts';
+import AboutUs from './components/AboutUs';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { Footer } from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Homepage/>}></Route>
+        <Route path="/cohorts" element={<Cohorts />}></Route> 
+        <Route path="/aboutus" element ={<AboutUs />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>  
+      <Footer/>   
     </div>
   );
 }
