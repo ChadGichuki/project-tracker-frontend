b2cf69c3ee927d5f06c7de15a10e96684a8cdeda
ClementNjeru <njeruclement36@gmail.com>
2022-10-25 10:37:36 +0300

    creates the SignUp page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import Slider from './Slider'
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
 
-function SignUp() {
+function App() {
   return (
-    <div>SignUp
-      <Slider />
-    </div>
-  )
+    <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp)'}}>
+      <div className='mask gradient-custom-3'></div>
+      <MDBCard className='m-5' style={{maxWidth: '600px', borderRadius: "25px"}}>
+        <MDBCardBody className='px-5'>
+          <h2 className="text-uppercase text-center mb-5 text-warning">Student Sign Up</h2>
+          <MDBInput wrapperClass='mb-4' label='Your Name' size='lg' id='form1' type='text'/>
+          <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email'/>
+          <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form3' type='password'/>
+          <MDBInput wrapperClass='mb-4' label='Repeat your password' size='lg' id='form4' type='password'/>
+          <div className='d-flex flex-row justify-content-center mb-4'>
+            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' />
+          </div>
+          <MDBBtn id='btn1' size='lg'>Sign Up</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBContainer>
+  );
 }
 
-export default SignUp
+export default App;
