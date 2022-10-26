80e2560cbea516fe39ac520814cf23e11a03760e
chrispus1000 <minrevac@gmail.com>
2022-10-26 08:32:23 +0300

    chrispus_feature Cohorts

--- src/components/Cohorts.js
+++ src/components/Cohorts.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Cohorts() {
+  const [cards] = useState([
+    {
+      title: 'card 1'
+    }
+  ])
   return (
-    <div>Cohorts</div>
+    <div>
+      <section>
+        <div classname="container">
+          <h1>Explore projects by different cohorts</h1>
+          <div classname="cards">
+            <div classname="cards">
+              <h3>card 1</h3>
+              <p>
+                Name: SD59-63
+                Date: Nov 2020-Dec 2021
+
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
   )
 }
 
