21ed05a786d8b6057eb640d2b04691857ff95b0b
chrispus1000 <minrevac@gmail.com>
2022-10-27 08:37:19 +0300

    cohort.js

--- src/components/Cohorts.js
+++ src/components/Cohorts.js
@@ -1,25 +1,85 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import '../components/Cohort.css'
 
 function Cohorts() {
+  const[projects, setProjects] = useState([])
+  const[, setProject_id] = useState(null)
+
+
+  useEffect(()=> {
+    fetch("https://project-tracker-phase5.herokuapp.com/")
+    .then((res) => res.json())
+    .then((data) => {
+      setProjects(data)
+      setProject_id(projects[0].id)
+    })
+  }, [projects])
+
+
   const [cards] = useState([
     {
-      title: 'card 1'
-    }
+      
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
+    {
+     
+      text: 'Name Date'
+    },
+    {
+     
+      text: 'Name Date'
+    },
+    {
+      
+      text: 'Name Date'
+    },
   ])
   return (
     <div>
       <section>
-        <div classname="container">
+        <div className="container">
           <h1>Explore projects by different cohorts</h1>
-          <div classname="cards">
-            <div classname="cards">
-              <h3>card 1</h3>
+          <div className="card">
+            {
+              cards.map((card, i) => (
+             
+            <div key={i} className="cards">
+              
+             
               <p>
                 Name: SD59-63
                 Date: Nov 2020-Dec 2021
 
               </p>
+              <button className="btn">view projects</button>
             </div>
+               
+               ))
+              }
           </div>
 
         </div>
