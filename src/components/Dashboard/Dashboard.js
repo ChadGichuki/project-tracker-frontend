239a2e54d92bd40efe0f8256116e3d01b60cd8d0
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-10-29 08:42:51 -0400

    improved design

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,42 @@
 import React, { useEffect, useState,  } from 'react'
-// import {useform} from 'react-hook-form'
-// import { Link, Route } from 'react-router-dom';
-// import { Link, useParams } from "react-router-dom";
-// import Members from '../about/Members'
-// import Projectdetails from './Projectdetails';
-import './dashboard.css'
- 
-function Dashboard() {
+// import { Modal } from 'react-bootstrap'
+import {
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
+import './dashboard.css';
+
+ function Dashboard () {
+
+
+  const [menuCollapse, setMenuCollapse] = useState(false)
+  const menuIconClick = () => {
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+    // const { collapseSidebar, toggleSidebar } = ProSidebar();
+  };
+
+
+  // const { id, name, description, github_link, category, cohort_id, user_id, users } = project
+  // const [projectDetails, setProjectDetails] = useState({
+  //     "users": [{"name": "Loaded"}]
+  
     // const [page, setPage] = useState("List");
     // const [dashboard, setDashboard] = useState([]);
     // const [project, setProjects] = useState([]);
     // const [member, setMembers] = useState([]);
 // const id = useParams()['id']
 const token = localStorage.getItem('token')
+
+const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
 
 useEffect(()=> {
    fetch('https://project-tracker-phase5.herokuapp.com/projects',{
@@ -46,64 +70,48 @@ useEffect(()=> {
 //         console.log("new member :", newMember);
 //     }
 
-
-return(
-<div id="viewport">
- 
-  <div id="sidebar">
-    <header>
-      <a href="#Project Tracker">...</a>
-    </header>
-    <ul class="nav">
-      <li>
-        <a href="/">
-          <i class="zmdi zmdi-view-dashboard"></i> My Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="/">
-          <i class="zmdi zmdi-link"></i> Add Project
-        </a>
-      </li>
-      <li>
-        <a href="/">
-          <i class="zmdi zmdi-widgets"></i> Update Project
-        </a>
-      </li>
-
-      <li> 
-        <a href="/">
-          <i class="zmdi zmdi-info-outline"></i> Add Members
-        </a>
-      </li>
-    </ul>
-  </div>
-
-  <div id="content">
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="/"><i class="zmdi zmdi-notifications text-danger"></i>
-            </a>
-          </li>
-          <li><a href="/">Notifications</a></li>
-        </ul>
-      </div>
-    </nav>
-    <div class="container-fluid">
-      <h1>This is my dashboard</h1>
-      <p>
-        It is empty for now but, watch this space.
-      </p>
+return (
+   <>
+    <div id="dashboard">
+        {/* collapsed props to change menu size using menucollapse state */}
+      <ProSidebarProvider collapsed={menuCollapse}>
+        {/* <SidebarHeader> */}
+        <div className="header">
+            {/* small and big change using menucollapse state */}
+            <p>{menuCollapse ? "DashBoard" : "My DashBoard"}</p>
+          </div>
+          <div className="closemenu" onClick={menuIconClick}>
+              {/* changing menu collapse icon on click */}
+            {menuCollapse ? (
+              <FiArrowRightCircle/>
+            ) : (
+              <FiArrowLeftCircle/>
+            )}
+          </div>
+        {/* </SidebarHeader> */}
+        {/* <SidebarContent> */}
+          <Menu iconShape="square">
+            <MenuItem active={true} icon={<FiHome />}>
+              Home
+            </MenuItem>
+            <MenuItem icon={<FaList />}>Add Projects</MenuItem>
+            <MenuItem icon={<FaRegHeart />}>Update Projects</MenuItem>
+            <MenuItem icon={<RiPencilLine />}>Add Members</MenuItem>
+            <MenuItem icon={<BiCog />}>Settings</MenuItem>
+          </Menu>
+        {/* </SidebarContent> */}
+        {/* <SidebarFooter> */}
+          <Menu iconShape="square">
+            <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+          </Menu>
+        {/* </SidebarFooter> */}
+      </ProSidebarProvider>
     </div>
-  </div>
-</div>
- 
+  </>
 );
-}
-// }
- 
- 
- 
-export default Dashboard;
+};
+
+   
+   
+   
+  export default Dashboard;
